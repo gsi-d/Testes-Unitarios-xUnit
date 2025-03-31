8e088ae627a2e00a94407aa73dd15a3bd89c47b2
Guilherme Felipe <guimota_silver@hotmail.com>
2025-03-31 08:53:55 -0300

    Adicionar arquivos de projeto.

---
+++ IntroducaoTestesUnitarios/UnitTest1.cs
@@ -0,0 +1,36 @@
+namespace IntroducaoTestesUnitarios
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FallingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void Theory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+    }
+}
